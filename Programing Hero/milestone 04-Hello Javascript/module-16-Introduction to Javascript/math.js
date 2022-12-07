435aa59f9c64b86de42f7bbdad7c0428049bfa2b
KAWSAR KABIR <kawsarkabir11@gmail.com>
2022-12-08 00:04:45 +0600

    five things you need to declare a variable

---
+++ Programing Hero/milestone 04-Hello Javascript/module-16-Introduction to Javascript/math.js
@@ -0,0 +1,13 @@
+
+console.log("here is needed math for programming");
+/**
+ * 1.Number ==> 55, 55.56, 777, -32
+ * 2. Integer ==> 2, 5
+ * 3.Float ==> 3.5 , 80.99
+ * 4.operatio ==> addition, substraction, multilication, divition, remainder
+ * 5.percent
+ * 6.average
+ * 7.comparison ==> >, <, ==, >=, <=
+ * 8.oikikNiyom ==> 2kilo apple & 5kilo mango
+ * 9. 
+ *   **/
