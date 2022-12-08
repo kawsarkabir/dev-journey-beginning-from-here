862c069947365f6a4eab48bfb00f2f60f76d3f80
KAWSAR KABIR <kawsarkabir11@gmail.com>
2022-12-09 01:02:41 +0600

    caomparison operator

---
+++ Programing Hero/milestone 04-Hello Javascript/module-17- Fundamental Concepts Array & Conditional/comparison.js
@@ -0,0 +1,17 @@
+//  17-5 Compare variables and Comparison operator
+// console.log(5 < 6);
+// console.log( 5 > 6)
+// console.log(5 == 6);
+// console.log(6==5);
+// console.log( 5 != 6);
+// console.log(15 != 15);
+// console.log( 5 <= 6);
+// console.log( 5>= 6);
+// console.log( 5>= 2);
+
+
+// multiple condition 
+
+// money1 > money2 && result1 > result2 && height1 > height2 
+
+// money1 > money2 || result1 > result2 || height1 > height2
---
+++ Programing Hero/milestone 04-Hello Javascript/module-17- Fundamental Concepts Array & Conditional/push-pop.js
@@ -0,0 +1,14 @@
+// 17-4 Add or remove element from array using push, pop
+
+
+var myFriends = ["kew nai", "ami", "tumi", "se"];
+
+// array last element hisabe add korate .push use kora lage 
+myFriends.push("janu")
+
+var numbers = [1, 3, 4, 5, 9];
+
+// last element hisabe remove korte use korte hoi
+numbers.pop()
+console.log(myFriends);
+console.log(numbers);
