e077a9bb26c699b8991682f85c50290912880eb7
KAWSAR KABIR <kawsarkabir11@gmail.com>
2022-12-09 00:10:05 +0600

    array length , index

---
+++ Programing Hero/milestone 04-Hello Javascript/module-17- Fundamental Concepts Array & Conditional/array.js
@@ -0,0 +1,8 @@
+// 17-2 Declare Array, array length and array index
+
+var friendAge = [11, 12, 13, 45, 66, 70];
+var actress = ["sabana", "sabnur", "opu"];
+// console.log( friendAge);
+console.log(actress);
+//  array ar position suru hoi all time 0 theke
+
