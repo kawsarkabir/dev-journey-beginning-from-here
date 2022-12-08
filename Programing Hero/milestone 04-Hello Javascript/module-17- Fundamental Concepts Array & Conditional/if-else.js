b25eac0b4cbf58a547a7c1a4546660438d66c2a0
KAWSAR KABIR <kawsarkabir11@gmail.com>
2022-12-09 01:15:55 +0600

    conditional if-else

---
+++ Programing Hero/milestone 04-Hello Javascript/module-17- Fundamental Concepts Array & Conditional/if-else.js
@@ -0,0 +1,19 @@
+// 17-6 Make conditional decision, if-else, comparison
+
+var iphonePrice = 70000;
+var myBudget = 100000;
+if (myBudget < iphonePrice) {
+    console.log('ami fokir vai ');
+}
+else{
+    console.log('iphone kine futani marbo');
+}
+
+// >=, <= conditional
+var bookPrice = 300;
+var myTk = 300;
+if (myTk >= bookPrice) {
+    console.log(" i will buy this book");
+} else {
+    console.log('aibar o fokir ami');
+}
