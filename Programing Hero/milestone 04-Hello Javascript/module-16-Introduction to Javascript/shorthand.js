01c80db61f7aff7bbe28d9df7658bb184b750bba
KAWSAR KABIR <kawsarkabir11@gmail.com>
2022-12-08 16:37:28 +0600

    shorthand

---
+++ Programing Hero/milestone 04-Hello Javascript/module-16-Introduction to Javascript/shorthand.js
@@ -0,0 +1,19 @@
+var tomatoPrice = 30;
+// variable value update
+var tomatoPrice = 25;
+
+// add something to the previous value
+
+var price1 =31;
+var price2 = 10;
+
+
+// set  a new value 
+price1 = price1 + 35;
+console.log(price1);
+// += 5 means joto value ase jok hoye jabe -= both are same 
+
+
+// ++ means adding 1 to the exiting value
+var age++;
+
