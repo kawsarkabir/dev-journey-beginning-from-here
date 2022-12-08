867a7f43da321e590597f69aeba55e38e71777a4
KAWSAR KABIR <kawsarkabir11@gmail.com>
2022-12-08 16:58:38 +0600

    parseInt, ParseFlot

---
+++ Programing Hero/milestone 04-Hello Javascript/module-16-Introduction to Javascript/concatenation.js
@@ -0,0 +1,10 @@
+var price1 = 30;
+var price2 = 40;
+var totalPrice = price1 + price2;
+// console.log(totalPrice);
+
+
+var fristName = "kawsar"
+var lastName = "kabir"
+var fullName = fristName  + " " + lastName; //this is concatation
+console.log(fullName);
---
+++ Programing Hero/milestone 04-Hello Javascript/module-16-Introduction to Javascript/conversion.js
@@ -0,0 +1,15 @@
+var price1 = 30;
+var price2 = '30';
+var totalPrice = price1 + price2;
+console.log(totalPrice);
+
+// convert string to number
+
+console.log(price1);
+var price1Number = parseInt(price2)
+console.log(price1Number);
+
+// parsefloat
+
+
+
