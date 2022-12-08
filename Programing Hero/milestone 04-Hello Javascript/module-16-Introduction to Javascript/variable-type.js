2333707335b43642c88e8e7845ffde3784c5812d
KAWSAR KABIR <kawsarkabir11@gmail.com>
2022-12-08 14:33:52 +0600

    variable type

---
+++ Programing Hero/milestone 04-Hello Javascript/module-16-Introduction to Javascript/variable-type.js
@@ -0,0 +1,6 @@
+// 16-4 Variable type, Numeric, String, Boolean
+
+var person = "k2"; //this is str type 
+var myAge = 17; //this is integer type
+var myResult = 3.65; //this is float type 
+var isSingle = true; // 
