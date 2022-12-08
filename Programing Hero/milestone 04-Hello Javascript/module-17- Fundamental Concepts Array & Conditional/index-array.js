103e5f71dfaac3f54c5888452d022e2cbd073a4a
KAWSAR KABIR <kawsarkabir11@gmail.com>
2022-12-09 00:24:20 +0600

    array index, get and set by index , indexof

---
+++ Programing Hero/milestone 04-Hello Javascript/module-17- Fundamental Concepts Array & Conditional/index-array.js
@@ -0,0 +1,12 @@
+// 17-3 Array index, get and set by index, indexOf
+
+var numbers = [34, 50, 44, 30, 20, 10];
+
+// get element by index
+console.log((numbers[3]));
+
+// set element by array
+numbers [0] = 700;
+console.log(numbers);
+var positionIndex = numbers.indexOf(500);
+console.log(positionIndex);
