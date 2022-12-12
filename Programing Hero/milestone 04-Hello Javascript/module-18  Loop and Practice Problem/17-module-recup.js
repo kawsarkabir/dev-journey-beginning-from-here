e4192f29058d11a3a79cefb5ef5575f83e357b7e
KAWSAR KABIR <kawsarkabir11@gmail.com>
2022-12-12 22:47:43 +0600

    17 module recup

---
+++ Programing Hero/milestone 04-Hello Javascript/module-18  Loop and Practice Problem/17-module-recup.js
@@ -0,0 +1,21 @@
+// 18-1 Variable array and conditionals revision
+
+// string type variable
+var bottle = "water";
+// same
+var bottleColor ="yellow";
+// number type variable type
+var sunglassPrice = 120;
+// 
+var pencolor = "blue";
+// bolean type variale
+var isExpensive = true;
+
+
+// array recup
+
+var tableItems = ["bottle", "pen", "mouse", "sunglass"];
+// array te koita element ase seta ber kora 
+tableItems.length
+console.log(tableItems.length);
+
