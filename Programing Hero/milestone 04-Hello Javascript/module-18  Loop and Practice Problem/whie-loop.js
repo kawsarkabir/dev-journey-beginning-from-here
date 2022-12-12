3fd7511f87fadd53d595d36c70caa3f75676b120
KAWSAR KABIR <kawsarkabir11@gmail.com>
2022-12-12 23:05:10 +0600

    while loop

---
+++ Programing Hero/milestone 04-Hello Javascript/module-18  Loop and Practice Problem/whie-loop.js
@@ -0,0 +1,9 @@
+//  18-2 While loop, debug and understand while loop
+
+var roastGiven = 0;
+while(roastGiven <=500 ){
+    console.log("I Love You");
+    console.log(roastGiven);
+    roastGiven++;
+}
+
