0f7a6ea905e45776a09093f0640783a94e76bf08
KAWSAR KABIR <kawsarkabir11@gmail.com>
2022-12-13 22:28:03 +0600

    for loop

---
+++ Programing Hero/milestone 04-Hello Javascript/module-18  Loop and Practice Problem/run-for-loop.js
@@ -0,0 +1,19 @@
+// for( var i = 1; i <= 10; i++){
+//    console.log(i);
+// }
+
+// target : display every element of an array
+
+var numbers = [45, 50, 90, 28, 29,20,48];
+
+for(var i= 0; i<7; i++){
+    number=numbers[i];
+    console.log(number);
+}
+
+// another 
+var items =["bottle", "mouse", "sunglas", "pen", "k2" ];
+for(var i = 0; i <items.length; i++){
+   var item = items[i];
+    console.log(item);
+}
