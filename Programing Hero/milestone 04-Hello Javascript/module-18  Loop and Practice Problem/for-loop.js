50a45d835acde7cb8d4c81a59add2c659871cc61
KAWSAR KABIR <kawsarkabir11@gmail.com>
2022-12-13 19:51:47 +0600

    for loop

---
+++ Programing Hero/milestone 04-Hello Javascript/module-18  Loop and Practice Problem/for-loop.js
@@ -0,0 +1,20 @@
+// 18-4 For loop, how for loop works, while vs for loop
+
+
+// this is while loop
+// var roastGiven = 0;
+// while(roastGiven<=10){
+//     console.log("amare roast den vai");
+//     roastGiven++;
+// }
+
+// this is for loop
+// for(var roastGiven = 0; roastGiven<=10; roastGiven++){
+//     console.log("Roast Den Please !");
+// }
+
+
+// for loop in here
+for(var i =0; i < 10; i+=2){
+    console.log(i);
+}
