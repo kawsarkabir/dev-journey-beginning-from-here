9a3b468644060268ecb8eedca809e40ead8ca192
KAWSAR KABIR <kawsarkabir11@gmail.com>
2022-12-13 19:15:06 +0600

    even, odd, while loop in js

---
+++ Programing Hero/milestone 04-Hello Javascript/module-18  Loop and Practice Problem/even.js
@@ -0,0 +1,19 @@
+//  18-3 Loop Example numbers, odd numbers, even numbers
+
+
+// find the even number in while loop
+
+// var number = 1;
+// while(number<=10){
+//     console.log(number);
+//     number++;
+//     number= number+ 1;
+// }
+
+// find the odd number in while loop
+var number = 0;
+while (number <= 10){
+    console.log(number);
+    number = number + 1;
+    
+}
