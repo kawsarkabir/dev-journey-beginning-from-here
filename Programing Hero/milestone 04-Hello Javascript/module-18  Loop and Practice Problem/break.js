8bd2bb9b27a502c495e89b82ee324137ed4e1622
KAWSAR KABIR <kawsarkabir11@gmail.com>
2022-12-16 09:40:44 +0600

    break

---
+++ Programing Hero/milestone 04-Hello Javascript/module-18  Loop and Practice Problem/break.js
@@ -0,0 +1,23 @@
+//   18-6 (advanced) while and for loop break and continue
+
+
+
+// recup
+// var friends =['ami ', 'tumi', 'se'];
+// for(var i = 0; i< friends.length; i++){
+//     var friend = friends[i];
+//     console.log(friend);
+// }
+
+for (var i = 0; i <= 20; i++){
+    console.log(i);
+    if(i>16){
+        break;
+    }
+}
+
+var roastedGiven = ['kawsar', 'bawsar', 'lawsar'];
+while(roastedGiven < 10){
+    console.log('i');
+    roastedGiven++;
+}
