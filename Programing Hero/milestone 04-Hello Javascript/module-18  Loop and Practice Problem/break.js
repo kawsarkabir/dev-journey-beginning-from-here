2e55939ed8f595492b079e878949d1efa2bf4e04
KAWSAR KABIR <kawsarkabir11@gmail.com>
2022-12-17 12:28:21 +0600

    variable

--- Programing Hero/milestone 04-Hello Javascript/module-18  Loop and Practice Problem/break.js
+++ Programing Hero/milestone 04-Hello Javascript/module-18  Loop and Practice Problem/break.js
@@ -9,15 +9,31 @@
 //     console.log(friend);
 // }
 
-for (var i = 0; i <= 20; i++){
-    console.log(i);
-    if(i>16){
+// for (var i = 0; i <= 20; i++){
+//     console.log(i);
+//     if(i>16){
+//         break;
+//     }
+// }
+
+// while loop in break 
+
+// var roastedGiven = ['kawsar', 'bawsar', 'lawsar'];
+var roastedGiven = 0;
+while(roastedGiven < 10){
+    console.log('vai amare den vai');
+    roastedGiven++;
+    
+    if(roastedGiven> 7){
         break;
     }
 }
 
-var roastedGiven = ['kawsar', 'bawsar', 'lawsar'];
-while(roastedGiven < 10){
-    console.log('i');
-    roastedGiven++;
+//  another 
+var items = ['bottle', 'mouse', 'sunglass', 'notebook', 'pen'];
+for(var i = 0; i < items.length ; i++){
+    var item = items[i];
+    
+    console.log(item);
 }
+
