6a7a2506f2339d3ce46c6462163860b1018bc796
KAWSAR KABIR <kawsarkabir11@gmail.com>
2023-01-27 23:03:46 +0600

    matha oparation shorthand

---
+++ Programing Hero/milestone 04-Hello Javascript/module-16-Introduction to Javascript/module-16-recup.js
@@ -0,0 +1,25 @@
+//  17-7 (advanced) Mathematical operation shorthand
+
+// sumation ar shortthand
+var tomatoPrice = 30;
+price1 = 20;
+// tomatoPrice = tomatoPrice + 20;
+price1 += 10;
+console.log(tomatoPrice);
+
+// substraction ar sorthand 
+// -= means deductiong some values form the curent valie og the var
+price2 = 70;
+price2 -= 40
+console.log(price2);
+//  *=
+price2*= 5
+
+//  /=
+price1 /= 5;
+console.log(price1)
+
+// ++ means adding to the exiting value
+
+
+// -- means sunsctruction ot the wxiting value 
