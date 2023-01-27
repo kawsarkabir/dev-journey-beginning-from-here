7cb6b90e02effb26bd1f01bfe28515b5aa7bbdd1
KAWSAR KABIR <kawsarkabir11@gmail.com>
2023-01-28 00:11:34 +0600

    practice problem

---
+++ Programing Hero/milestone 04-Hello Javascript/module-16-Introduction to Javascript/problem.js
@@ -0,0 +1,20 @@
+// proble number no.1
+var momGive = 1000;
+orangePrice = 400;
+applePrice = 300;
+totalCost = applePrice + orangePrice;
+havingTk= momGive - totalCost;
+console.log(havingTk);
+
+// problem number 2.
+var mathematicsMark = 75.25;
+var biologyMark = 65;
+var chemistryMark = 80;
+var physicsMark = 35.45;
+var banglaMark = 99.50;
+totalMark = mathematicsMark + biologyMark + chemistryMark + physicsMark + banglaMark;
+avarageMark = totalMark / 5;
+console.log(avarageMark.toFixed(2));
+
+// problem 3
+
