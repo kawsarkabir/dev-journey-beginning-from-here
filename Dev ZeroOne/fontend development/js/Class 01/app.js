cc762e0c9d688a3729ae51fc2c15732910fb910e
KAWSAR KABIR <kawsarkabir11@gmail.com>
2023-01-29 15:06:58 +0600

    output

---
+++ Dev ZeroOne/fontend development/js/Class 01/app.js
@@ -0,0 +1,9 @@
+//js output show in 4 way
+
+document.getElementById("demo").innerHTML = 'eta document.getElementById.innerHTML diye dekha output';
+document.write('eta doument.write diye output');
+alert('Hello! I am alert');
+console.log('I am console.log');
+
+
+ 
