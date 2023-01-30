673f00afc49fc7867f170d02e63be326beab7ff1
KAWSAR KABIR <kawsarkabir11@gmail.com>
2023-01-30 20:03:23 +0600

    js statement with sumit vai

---
+++ Dev ZeroOne/fontend development/js/Class 02/statements.js
@@ -0,0 +1,20 @@
+// statemate is write recomended
+// js akta matro semicolon kei se count korbe but ar beshi korbe na 
+// oparator is +-*/
+
+// what is indention ? as like as alignment thik rakhe  
+
+
+/* 
+what is keyword ?
+var 
+let const
+if 
+switch
+int
+array
+for
+function
+return
+
+*/
