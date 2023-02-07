4510875731e87592006672bb3f3294f4c3cceb48
KAWSAR KABIR <kawsarkabir11@gmail.com>
2023-02-07 14:58:18 +0600

    fuction declare

---
+++ Programing Hero/milestone 04-Hello Javascript/module 19- Core Concept Function and Object/function.js
@@ -0,0 +1,8 @@
+// 19-2 Declare a Function, call function, function vs loop
+
+function startFan(){
+    console.log('Stand up');
+    console.log('walk towards the switch');
+    console.log('press the switch');
+}
+startFan()
