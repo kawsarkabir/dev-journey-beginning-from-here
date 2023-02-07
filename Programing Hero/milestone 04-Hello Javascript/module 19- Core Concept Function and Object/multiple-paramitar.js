69a623aa3889a51a1479ff413f45f1ad0e76dc76
KAWSAR KABIR <kawsarkabir11@gmail.com>
2023-02-07 15:19:51 +0600

    multiple paramitar

---
+++ Programing Hero/milestone 04-Hello Javascript/module 19- Core Concept Function and Object/multiple-paramitar.js
@@ -0,0 +1,15 @@
+// 19-3 Add Function parameter, handle multiple parameters
+
+/* function bringSingara(){
+    console.log('ai nen singara');
+}
+bringSingara() ;
+ */
+
+
+function sum(a, b, c, d, e){
+    console.log(a, b, c, d, e);
+    var sum = a + b + c + d + e;
+    console.log(sum);
+}
+sum(1, 2, 3, 4, 5)
