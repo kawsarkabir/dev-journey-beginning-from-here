e17277a6b2bed9f9691cf20041537f0516742add
KAWSAR KABIR <kawsarkabir11@gmail.com>
2023-02-10 18:35:34 +0600

    function declare and parameter

---
+++ Programing Hero/milestone 04-Hello Javascript/module 19- Core Concept Function and Object/recup-19.js
@@ -0,0 +1,37 @@
+// 19-2 Declare a Function, call function, function vs loop
+
+/* function startFan(){
+    console.log('Stand up');
+    console.log('walk towards the switch');
+    console.log('press tha switch');
+}
+startFan();
+ */
+
+//  parameters 
+/* function bringSingra(money){
+    console.log('singara den mama');
+    console.log(money);
+    console.log('ai nen singara');
+
+}
+bringSingra(300) */
+/* function bringSingra(money){
+    console.log('eto tk disen',  money);
+    console.log('ai nen singara');
+}
+//===================================================
+var taka = 300;
+bringSingra(taka); */
+
+//2 number add function
+/* function add(num1, num2){
+    sum = num1 + num2;
+    console.log(sum);
+}
+add(5 , 7) */
+
+
+// ===============================================
+
+
