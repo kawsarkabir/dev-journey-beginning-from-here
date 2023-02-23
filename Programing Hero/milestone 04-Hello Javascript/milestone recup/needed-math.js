1cd11c5c9f50fbe5253af3838daac89c0bbe40a3
KAWSAR KABIR <kawsarkabir11@gmail.com>
2023-02-23 10:09:34 +0600

    needed math for programming

---
+++ Programing Hero/milestone 04-Hello Javascript/milestone recup/needed-math.js
@@ -0,0 +1,16 @@
+/* 
+1. number ==> 55, 800, -32
+2.integer ==> 3, 4, 5, even, odd
+3. float ==> decimal 3.2 , 55.8
+d.oparation ==> addition , substraction, multipication, divition, remainder
+5. parcent
+6. avaraage
+7.comparison ==> >, <, ==, >=, <=, && , ||
+8. oikik ==> 2kg apple and 5 kilo angur total koto cost ?
+9. 45, 34, 89,29,90 : max, min avarage ?? 
+10. series relatave
+===========EXTRA=================
+1.prime
+2.permutation
+always ==> jodi lage tahle sikhiye nibo
+*/
