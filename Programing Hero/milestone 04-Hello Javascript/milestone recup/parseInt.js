38474528cf32027152ea0ac57c0528f83b0c9096
KAWSAR KABIR <kawsarkabir11@gmail.com>
2023-02-23 12:37:50 +0600

    module 16 finished

---
+++ Programing Hero/milestone 04-Hello Javascript/milestone recup/parseInt.js
@@ -0,0 +1,19 @@
+/* var demo1 = 31;
+var demo2 = 10;
+var totalDemo = demo1 + demo2; */
+
+
+/* var fristName = 'kawsar';
+var lastName = 'kabir';
+var fullName = fristName + ' ' + lastName;
+console.log(fullName); */
+
+var price1 = 31;
+var price2 = '10'
+var sum = parseFloat( price1 + price2)
+console.log(sum);
+
+// type of javascript
+
+console.log(typeof(sum));
+
