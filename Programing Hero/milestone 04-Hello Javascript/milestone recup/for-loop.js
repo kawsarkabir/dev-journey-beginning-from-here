2e250dce583205766a97deb5909ce358e36d14fa
KAWSAR KABIR <kawsarkabir11@gmail.com>
2023-02-24 20:50:41 +0600

    for loop

---
+++ Programing Hero/milestone 04-Hello Javascript/milestone recup/for-loop.js
@@ -0,0 +1,27 @@
+// for loop declaretion
+for( var i = 1; i <= 10; i++){
+    // console.log(i);
+}
+
+// even number find using for loop
+for(var i = 0; i<=50; i+= 2){
+    // console.log(i);
+}
+//  oodd number find usinf for loop
+for(var i = 1; i<=50; i+=2){
+    // console.log(i);
+}
+
+// display every elements of array
+let arrys = [0, 2, 3, 4, 5, 6, 7];
+for(var i = 0; i<arrys.length; i++){
+    var arry = arrys[i]
+    console.log(arry);
+}
+
+
+var items = ['bottle', 'sunglass', 'moneyback', 'mouse'];
+for(var i = 0; i < items.length; i++){
+    var item = items[i]
+    console.log(item);
+}
