2a61dda2f00274015336fbb1fd44fec97aee3e88
KAWSAR KABIR <kawsarkabir11@gmail.com>
2023-02-24 20:34:58 +0600

    while loop

---
+++ Programing Hero/milestone 04-Hello Javascript/milestone recup/while-loop.js
@@ -0,0 +1,21 @@
+var roastGiven = 1;
+while(roastGiven <= 10){
+    console.log(roastGiven);
+    roastGiven ++;
+}
+
+
+// find the odd number
+var oddNumber = 1;
+while(oddNumber <=20){
+    console.log(oddNumber);
+    oddNumber += 2
+}
+
+//  eeven number
+var evenNumber = 0;
+while(evenNumber <=10){
+   console.log(evenNumber);
+    evenNumber += 2
+   
+}
