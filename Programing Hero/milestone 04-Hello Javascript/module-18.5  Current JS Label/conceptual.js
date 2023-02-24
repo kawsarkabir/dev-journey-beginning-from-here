cf62e63cb48acccb938903844ade075c354c4be8
KAWSAR KABIR <kawsarkabir11@gmail.com>
2023-02-25 01:02:51 +0600

    conceptual session done

---
+++ Programing Hero/milestone 04-Hello Javascript/module-18.5  Current JS Label/conceptual.js
@@ -0,0 +1,87 @@
+// variable namiing convenstion
+
+// 1. letter(A-Z)
+// 2. number(0-9)
+// 3.Underscror(_)
+// 4. Dollar ($)
+
+// not allow in variable name
+// 1. keyword use kora jabe na pothome
+// 2. number fast a use kora jabe na
+// 3. space diya jabe na
+
+// some keyword 
+/* 
+break, do , in , else, in , typeof, ease, if, this, new, const, default, var, while, continue
+*/
+
+
+// data type in js . there are 2 type of js. 1, primitive, non promitive
+// primitive data type
+/* 
+1. string
+2. number
+3. boolean
+4. null
+5. undefinded
+*/
+// non primitive data type
+/* 
+1. object
+2. funcion
+3. array
+*/
+
+
+// type of example here
+
+/* var num = 20;
+var str = 'k2';
+var isGratuated = true;
+console.log(typeof(num));
+console.log(typeof(str));
+console.log(typeof(isGratuated)); */
+
+
+// parseInt && parseFlioat
+
+/* var number = 71.52
+console.log(parseFloat(number));
+console.log(parseInt(number)); */
+
+// if -else condition here
+/* let nayok = 'Hero Alom';
+if(nayok == 'Hero Alom'){
+    console.log('marbo bogurai lash porbe magurai');
+}
+else if( nayok == 'mithun'){
+    console.log('marbo kahne lash porbe soshane');
+}
+else{
+    console.log('bhai maramari kora akdom uchit na');
+} */
+
+
+//  grading systmes  useing if-else
+/* var result = 43;
+if(result < 33 && result > 0){
+    console.log('fail marcho');
+}
+else if(result >= 33 && result > 40){
+    console.log('tumi D Grade paico');
+}
+else if(result < 40 && result > 50){
+    console.log('tumi C Grade paico');
+} */
+
+// switch case example here
+
+var name = 'hero';
+switch (key) {
+    case value:
+        
+        break;
+
+    default:
+        break;
+}
