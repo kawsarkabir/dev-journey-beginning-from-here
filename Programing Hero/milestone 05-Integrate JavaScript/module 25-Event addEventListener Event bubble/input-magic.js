ced51989cf42087739953c2f7f1ab14353f85869
KAWSAR KABIR <kawsarkabir11@gmail.com>
2023-02-26 00:14:04 +0600

    test

---
+++ Programing Hero/milestone 05-Integrate JavaScript/module 25-Event addEventListener Event bubble/input-magic.js
@@ -0,0 +1,5 @@
+// option 2 recup 
+document.getElementById('btn-update').addEventListener('click', function(){
+  const inputField = document.getElementById('input-filed')
+  console.log('heloo');
+})
