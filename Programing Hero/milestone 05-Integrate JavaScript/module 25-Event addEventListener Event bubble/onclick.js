a0b0a10c3acc1abc01d5b7b78cd936786128f18e
KAWSAR KABIR <kawsarkabir11@gmail.com>
2023-02-25 19:59:17 +0600

    onclick

---
+++ Programing Hero/milestone 05-Integrate JavaScript/module 25-Event addEventListener Event bubble/onclick.js
@@ -0,0 +1,32 @@
+// onclick function click option 2
+function makeRed(){
+    document.body.style.backgroundColor = 'red';
+}
+function danger(){
+    alert('Valo Hoye Jaw Kawsar')
+}
+
+// option 3
+const makeBlueButton = document.getElementById('makeBlueBtn')
+makeBlueButton.onclick = makeBlue
+function makeBlue(){
+    document.body.style.backgroundColor='blue'
+}
+
+const purpleButton = document.getElementById('make-purple')
+purpleButton.onclick = makePurple
+function makePurple(){
+    document.body.style.backgroundColor = 'purple'
+}
+
+// option 4
+const pinkButton =document.getElementById('make-pink');
+pinkButton.addEventListener('click', makePink)
+function makePink(){
+    document.body.style.backgroundColor = 'pink'
+}
+
+// option 4 fianl recomended
+document.getElementById('make-orange').addEventListener('click', function(){
+    document.body.style.backgroundColor = 'orange'
+})
