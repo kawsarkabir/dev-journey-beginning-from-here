d6eccfe1f633d4c8f5663381b39cb09a6798a15b
KAWSAR KABIR <kawsarkabir11@gmail.com>
2023-02-26 16:51:30 +0600

    more event

---
+++ Programing Hero/milestone 05-Integrate JavaScript/module 25-Event addEventListener Event bubble/more-event.js
@@ -0,0 +1,25 @@
+// click event
+document.getElementById('btn-more').addEventListener('click' , function(){
+    console.log('click');
+})
+// mousemove
+document.getElementById('btn-more').addEventListener('mousemove' , function(){
+    console.log('click');
+})
+// mouse enter
+document.getElementById('btn-more').addEventListener('mouseenter' , function(){
+    console.log('mouseenter');
+})
+// foucus  
+document.getElementById('text-field').addEventListener('focus' , function(){
+    console.log('focus ar por kaz korbe');
+})
+// blue
+document.getElementById('text-field').addEventListener('blur' , function(){
+    console.log('blue hosse');
+})
+// keydown
+document.getElementById('text-field').addEventListener('keydown' , function(){
+    console.log('keydown hosse');
+})
+
