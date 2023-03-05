92d32f1ec539d4b4efc6987685001e0b00853dfb
kawsarkabir <kawsarkabir11@gmail.com>
2023-03-05 21:35:54 +0600

    culculation done

--- Programing Hero/milestone 05-Integrate JavaScript/module 28-Shopping Cart and Pin Matcher/shopping-cart-solution/js/case.js
+++ Programing Hero/milestone 05-Integrate JavaScript/module 28-Shopping Cart and Pin Matcher/shopping-cart-solution/js/case.js
@@ -25,8 +25,10 @@ function updateCaseTotalPrice(newCaseNumber){
 document.getElementById('btn-case-plus').addEventListener('click', function(){
     const newCaseNumber = updateCaseNumber(true);
     updateCaseTotalPrice(newCaseNumber);
+    calculateSubTotal();
 });
 document.getElementById('btn-case-minus').addEventListener('click', function(){
     const newCaseNumber = updateCaseNumber(false);
     updateCaseTotalPrice(newCaseNumber);
+    calculateSubTotal();
 });
--- Programing Hero/milestone 05-Integrate JavaScript/module 28-Shopping Cart and Pin Matcher/shopping-cart-solution/js/phone.js
+++ Programing Hero/milestone 05-Integrate JavaScript/module 28-Shopping Cart and Pin Matcher/shopping-cart-solution/js/phone.js
@@ -16,14 +16,15 @@ function updatePhoneTotalPrice(newPhoneNumber){
     const phoneTotalPrice = newPhoneNumber * 1219;
     const phoneTotalElement = document.getElementById('phone-total');
     phoneTotalElement.innerText = phoneTotalPrice;
- }
-
+}
 
 document.getElementById('btn-phone-plus').addEventListener('click', function(){
     const newPhoneNumber = updatePhoneNumber(true);
     updatePhoneTotalPrice(newPhoneNumber);
+    calculateSubTotal();
 })
 document.getElementById('btn-phone-minus').addEventListener('click', function(){
      const newPhoneNumber = updatePhoneNumber(false);
      updatePhoneTotalPrice(newPhoneNumber)
+     calculateSubTotal();
 })
