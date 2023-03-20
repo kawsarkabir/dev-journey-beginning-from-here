9cfb5e191c22f7b2150b2762ce5a505bfac9bd86
kawsarkabir <kawsarkabir11@gmail.com>
2023-03-20 22:00:05 +0600

    tofixed

--- Javascript with anisul bhai/how to declare variables in JavaScript/apps.js
+++ Javascript with anisul bhai/how to declare variables in JavaScript/apps.js
@@ -1,7 +1,6 @@
 var namne = 'hello world';
 document.write(namne);
 
-
 //how to declare variables in JavaScript
 
 var ages // variale ar namer aage digit use kora jabe na
@@ -10,5 +9,27 @@ var friendAge // camelcale case vabe likhte recomend korse sovai evabei rakhe
 var name = 'k2';
 var age = 29;
 document.write(name, age);
-
 // number method | toFixed | toPrecision
+
+
+// ===================recup in anisul via 
+var name = 'recup';
+var age = 29;
+document.write(name, age);
+
+var name ='recup';
+// naming consvension in JavaScript
+
+// keyword use kora jabe na 
+// var return = 10;
+// number diye suru kora jabe na 
+// let 99ages = 99;
+
+// naming case sensitivity
+// const name = name
+let Age = 29;
+let age = 29;
+
+var name = 'kawsar kabir'
+var age = 19;
+console.log(name , age);
---
+++ Javascript with anisul bhai/how to declare variables in JavaScript/concat.js
@@ -0,0 +1,20 @@
+var num = '20';
+// console.log(typeof(num));
+
+// convert to number to str
+var number = 20;
+number = toString(number)
+// console.log(number);
+
+
+// toFixed notation
+var age = 2.3984;
+console.log(age.toFixed(2));
+
+// toPrecision notation
+var age = 29.3984;
+console.log(age.toPrecision(3));
+
+
+
+
