06dc63a156b46bde3c894d5fc2a659f248061e20
kawsarkabir <kawsarkabir11@gmail.com>
2023-03-20 15:34:21 +0600

    recup data type

---
+++ Javascript with anisul bhai/output/dataType.js
@@ -0,0 +1,37 @@
+// number type in data type
+var num1 = 10;
+console.log(num1);
+
+// string type in data type
+var str1 = "Hello";
+console.log(str1);
+
+// boolean type in data type
+var bool1 = true;
+console.log(bool1);
+
+// null type in data type
+var null1 = null;
+console.log(null1);
+
+// undefined type in data type  
+var undef1 = undefined;
+console.log(undef1);
+
+// object type in data type         
+var obj1 = {};
+console.log(obj1);
+
+// array type in data type          
+var arr1 = [];
+console.log(arr1);
+
+// function
+function add(a, b) {
+    return a + b;
+}
+console.log(add(10, 20));
+
+// arrow function
+const add1 = (a, b) => a + b;
+console.log(add1(10, 20));
---
+++ Javascript with anisul bhai/output/variable.js
@@ -0,0 +1,4 @@
+var age = 19;
+var myName = 'John';
+myName = 'kawsar kabir k2'
+console.log(age, myName);
