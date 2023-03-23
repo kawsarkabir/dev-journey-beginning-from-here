f0e277179e3fa4b09522330587574346c64af34e
kawsarkabir <kawsarkabir11@gmail.com>
2023-03-23 09:21:14 +0600

    recup

---
+++ Javascript with anisul bhai/recup/recup.js
@@ -0,0 +1,74 @@
+// how to show output in javascript 
+// 1. document.write('HEllo world');
+// console.log('HEllo world);
+// alert('HEllo world);
+// .innerHTML
+
+// how to add Javascript in html file
+/* 
+1, inline javascript (tag)
+2, internal javascript(head or body)
+3. Extarnal javascript (another file)
+*/
+
+// kewword data type and commnet 
+/* 
+some keyword in javascript: return , var, let , const, for, while, function, new, etc...
+*/
+// 
+// data type in javascript
+
+/* 
+let numberDataType = 10;
+console.log(numberDataType);
+*/
+// string dataType
+/* 
+let myName = 'kawsar kabir';
+console.log(myName);
+*/
+// bolean data Type
+/* 
+var isTrue = true;
+console.log(isTrue);
+*/
+
+
+// how to variable declaration in javascript
+var variableName  = 'kabir';
+console.log(variableName);
+
+// variable namming convention in javascript
+// var 99prices = true; // pthome number use kora jaba na eta wrong;
+// var while = 90; variable ar nam keywords use kora jaba na eta wrong
+
+// var #samble = '123' // fast a samble use kora jabe na but $ sing use kora jabe 
+
+
+// let variableName = 'kawsar';  recomendet eay camel case 
+
+// toFixed
+var num = 2;
+console.log(num);
+
+// convert number to string
+var convertTostring = 100;
+console.log(convertTostring.toString());
+
+// toFixed
+var number = 2.9040
+console.log(number.toFixed(2));
+
+//  to precision 
+var number1 = 2.9040;
+console.log(number1.toPrecision(2));
+
+// how to add or concatenate strings
+/* let name1 = 'kawsar';
+let name2 = 'kabir';
+let name = name1 + ' ' + name2;
+console.log(name); */
+
+
+
+
