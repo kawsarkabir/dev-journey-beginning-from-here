945303d9cb98daaaa0e68de08ae294011822a6b8
kawsarkabir <kawsarkabir11@gmail.com>
2023-03-25 00:36:33 +0600

    logical operator

--- Javascript with anisul bhai/recup/recup.js
+++ Javascript with anisul bhai/recup/recup.js
@@ -123,7 +123,7 @@ console.log(x *= y); */
 
 // make your own calculator
 
-var num1 = prompt('Enter Your Fast Number: ');
+/* var num1 = prompt('Enter Your Fast Number: ');
 var num2 = prompt('Enter Your Second Number: ');
 num1 = parseInt(num1);
 num2 = parseInt(num2);
@@ -131,23 +131,43 @@ var sum = num1 + num2;
 var sub = num1 - num2;
 var multiply = num1 * num2;
 var fagfol = num1 / num2;
-console.log(sum, sub, multiply, fagfol);
+console.log(sum, sub, multiply, fagfol); */
 
 // javascript bangla tutorial 11 : Area of various shapes
 
-var base = prompt('Enter Base: ')
+/* var base = prompt('Enter Base: ')
 var height = prompt('Enter Height: ')
 var area = base * height;
-console.log('Area = :' + area);
+console.log('Area = :' + area); */
  
 
 
 // javascript bangla tutorial 12 : how to make temperature converter
 
-var farenHeight = parseFloat(prompt('Enter the farenHeight: '))
+/* var farenHeight = parseFloat(prompt('Enter the farenHeight: '))
 var cels = (farenHeight - 32) * ( 5 / 9);
 console.log(cels);
 
 var cels = parseFloat(prompt('enter the celscias: '));
 var farenHeight = cels * (9/5) + 32;
-console.log(farenHeight);
+console.log(farenHeight); */
+
+// javascript bangla tutorial 13 : Relational and Logical Operator
+/* var num1 = 20;
+var num2 = 25;
+console.log(num1 > num2);
+console.log(num1 >= num2);
+console.log(num1 <= num2);
+console.log(num1 == num2);
+console.log(num1 === num2);
+console.log(num1 != num2); */
+
+
+// find the bigest number using javascript
+/* var number1 = 1;
+var number2 = 2;
+var number3 = 3;
+console.log(number1 > number2 && number3 > number2);
+//  or ar using ||
+console.log(number1 > number2 || number3 > number2); */
+
