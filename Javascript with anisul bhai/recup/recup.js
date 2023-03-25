9c4dafd50c03430a6d713b406d5f39f1c68f9c85
kawsarkabir <kawsarkabir11@gmail.com>
2023-03-25 15:07:27 +0600

    switch

--- Javascript with anisul bhai/recup/recup.js
+++ Javascript with anisul bhai/recup/recup.js
@@ -218,3 +218,81 @@ else{
     console.log('bhai tumi fail');
 } */
 
+// javascript bangla tutorial 16 : programs using logical operators
+
+
+// javascript bangla tutorial 17 : switch
+
+//  0- Zero, 1- 9 
+/* var digit = prompt('Enter any digit: ');
+if(digit == 0){
+    console.log('zero');
+}
+else if(digit == 1){
+    console.log('one');
+}
+else if(digit == 2){
+    console.log('two');
+}
+else if(digit == 3){
+    console.log('three');
+}
+else if(digit == 4){
+    console.log('four');
+}
+else if(digit == 5){
+    console.log('five');
+}
+else if(digit == 6){
+    console.log('six');
+}
+else if(digit == 7){
+    console.log('seven');
+}
+else if(digit == 8){
+    console.log('eight');
+}
+else if(digit == 9){
+    console.log('nine');
+}
+else{
+    console.log('not a valid number');
+} */
+
+
+// switch 
+v/* ar digit = prompt('enter any number');
+switch (digit){
+    case '0':
+        console.log('zero');
+        break;
+    case '1':
+        console.log('one');
+        break;
+    case '2':
+        console.log('two');    
+        break;
+    case '3':
+        console.log('three');
+        break;
+    case '4':
+        console.log('four');
+        break;
+    case '5':
+        console.log('five');
+        break;
+    case '6':
+        console.log('six');
+        break;
+    case '7':
+        console.log('seven');
+        break;
+    case '8':
+        console.log('eight');
+        break;
+    case '9':
+        console.log('nine');
+        break;
+    default:
+        console.log('not a number');
+} */
