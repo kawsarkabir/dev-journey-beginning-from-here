028b18d62a1e7b6792650384944aaedff6388f7f
kawsarkabir <kawsarkabir11@gmail.com>
2023-04-02 20:13:58 +0600

    create element by javascript

---
+++ Javascript with anisul bhai/event handler on click/app.js
@@ -0,0 +1,15 @@
+// style by javascript
+const link = document.getElementsByTagName('a')[0];
+link.innerHTML = 'Hello World';
+link.style.textDecoration = 'none';
+link.style.fontSize = '40px';
+link.style.color = 'red';
+// 
+var h1 = document.getElementsByTagName('h1')[0];
+h1.innerHTML = 'yeyo yeyo';
+
+// create element by javascript
+const newElement = document.createElement('h1');
+const newText = document.createTextNod('this is a pu pu');
+newElement.appendChild(newText);
+document.querySelector('.my-div');
