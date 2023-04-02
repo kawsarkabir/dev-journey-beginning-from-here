36e9f67fbc39b92115aa346ddfe8cd17f1246d36
kawsarkabir <kawsarkabir11@gmail.com>
2023-04-02 17:50:07 +0600

    add event handler onclick

---
+++ Javascript with anisul bhai/event handler on click/script.js
@@ -0,0 +1,19 @@
+var newPera = document.querySelector('#myimg');
+function myMessage1(){
+    newPera.src ='';
+}
+function myMessage2(){
+    newPera.innerText = 'i am message 2';
+}
+
+
+
+
+
+var newPera = document.querySelector('#myimg');
+function myMessage1(){
+    newPera.src ='https://lh3.googleusercontent.com/jUoaTIlBn5ibfQcND2n5OMD6Z7xoqNj-ShHlFR6QuLffLXD5pS8V2eNg1rGlrsRrnDkoQ28O8UHzqzBQKAGY4l1CS2NQSq2SkRScK6FOjl82jppyohK-';
+}
+function myMessage2(){
+     newPera.src = 'https://53.fs1.hubspotusercontent-na1.net/hub/53/hubfs/image8-2.jpg?width=595&height=400&name=image8-2.jpg"'
+}
