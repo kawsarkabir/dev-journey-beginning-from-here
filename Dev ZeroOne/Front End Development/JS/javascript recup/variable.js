2bda20ed42eb9065586706d6e5a53455d9a482a2
kawsarkabir <kawsarkabir11@gmail.com>
2023-04-09 23:45:34 +0600

    variable

---
+++ Dev ZeroOne/Front End Development/JS/javascript recup/variable.js
@@ -0,0 +1,15 @@
+console.log('10 + 50 = ' +(10+50));
+console.log('10 - 50 = '+(10-50));
+console.log('10 * 50 = ' +(10*50));
+console.log('10 / 50 = ' +(10/50));
+
+let a = 765;
+let b = 40;
+let c = a + b;
+console.log(a  + ' + ' + b + ' = ' + c);
+console.log(a  +' - ' + b + ' = ' + (a -b));
+console.log(a  + ' * ' + b + ' = ' + (a*b));
+console.log(a  + ' / ' + b + ' = ' + (a/b));
+
+
+
