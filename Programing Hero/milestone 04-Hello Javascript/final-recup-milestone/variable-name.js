d68ea5a3faddd12bae3f450cef57c97d7ea0d7e0
kawsarkabir <devkawsarkabir@gmail.com>
2023-04-12 19:56:48 +0600

    variable type recup

---
+++ Programing Hero/milestone 04-Hello Javascript/final-recup-milestone/variable-name.js
@@ -0,0 +1,15 @@
+// variable type 
+/* 
+1. number => int, float
+2. string
+3. bolean 
+*/
+
+let intResult = 4; // number typeee
+let floatResult = 4.00; // number typee
+
+// string typeeee
+let friends = 'Anamul';
+
+// bolean type 
+isSingle = true;
---
+++ Programing Hero/milestone 04-Hello Javascript/final-recup-milestone/variable.js
@@ -0,0 +1,10 @@
+console.log("Hello World !");
+
+// varibale naming convension
+let price = 110;
+console.log(price);
+
+let names = 'devkawsar kabir';
+console.log(names);
+
+// 
