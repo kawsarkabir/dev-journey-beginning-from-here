1bc4c9e22eb881d61f2562864e1b4b376d097b39
kawsarkabir <devkawsarkabir@gmail.com>
2023-04-15 02:46:40 +0600

    function

--- Programing Hero/milestone 04-Hello Javascript/final-recup-milestone/function.js
+++ Programing Hero/milestone 04-Hello Javascript/final-recup-milestone/function.js
@@ -35,3 +35,40 @@ function add (a, b, c, d, e){
 }
 add(1, 2, 3, 4, 5);
 
+// return 
+function added( num1, num2){
+    console.log(num1, num2);
+    let sum = num1 + num2;
+    console.log(sum);
+    return sum;
+}
+added(1, 2);
+
+// another retun function 
+let myTk = 200;
+function bringSingara2 (money){
+    let singaraPrice = 10;
+    let quantity = money / singaraPrice;
+    return quantity;
+}
+let singara = bringSingara2(myTk);
+console.log(singara);
+
+/* // function sumaryyyy && example 
+function functionName(perameters){
+    console.log('function body');
+    return;
+}
+functionName() //call function  */
+
+function getTotal(assingment1, assingment2, assingment3){
+    const total = assingment1 + assingment2 + assingment3;
+    const averages = total / 3;
+    return averages;
+}
+const assingment1Marks = 60;
+const assingment2Marks = 58;
+const assingment3Marks = 59;
+let myAverages = getTotal(assingment1Marks, assingment2Marks, assingment3Marks)
+console.log(myAverages);
+
