ce0506b78fa0b56e61500e0b40f40df809de61b7
kawsarkabir <devkawsarkabir@gmail.com>
2023-04-14 18:03:26 +0600

    for loop

--- Programing Hero/milestone 04-Hello Javascript/final-recup-milestone/loop.js
+++ Programing Hero/milestone 04-Hello Javascript/final-recup-milestone/loop.js
@@ -20,4 +20,27 @@ while(countNumber < 100){
     countNumber += 2;
 }
 
-// 
+// for loop
+for(cN = 50; cN < 100; cN++){
+    console.log(cN);
+}
+
+// odd number using for loop 
+for(odd =1; odd <= 20; odd+= 2){
+    console.log(odd);
+}
+
+
+// even number using for
+for(even = 0; even <= 50; even += 2){
+    console.log(even);
+}
+
+
+// sumation 1-10;
+
+sum = 0;
+for(let i = 1; i <= 10; i++){
+    sum = sum + i;
+    console.log(sum);
+}
