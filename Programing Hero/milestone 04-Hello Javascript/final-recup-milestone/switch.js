35487d09d18b17f0d5ff4700f26fe73ae21bf836
kawsarkabir <devkawsarkabir@gmail.com>
2023-04-15 03:45:10 +0600

    switch

---
+++ Programing Hero/milestone 04-Hello Javascript/final-recup-milestone/switch.js
@@ -0,0 +1,31 @@
+// switch vs if else
+/* let colors = 'green';
+if(colors == 'red'){
+    console.log('red color it is');
+}
+else if(colors == 'blue'){
+    console.log('you are my blue friend');
+}
+else if( colors == 'green'){
+    console.log('your are my green friend');
+}
+else{
+    console.log('you are my kala kala frieds');
+} */
+
+
+// switch 
+let friedsName = 'k2';
+switch(friedsName){
+    case 'rakib':
+        console.log('your name is rakib');
+        break;
+    case 'anamul':
+        console.log('your name is anamul');
+        break;
+    case 'k2':
+        console.log('Hello K2!');
+        break;
+    default:
+        console.log('kala kala friend');
+}
