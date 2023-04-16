45460f12b4c7d3afdcb6c54c58d586618fd5bb76
kawsarkabir <devkawsarkabir@gmail.com>
2023-04-17 02:49:33 +0600

    more event

--- Programing Hero/milestone 05-Integrate JavaScript/final recup/event.js
+++ Programing Hero/milestone 05-Integrate JavaScript/final recup/event.js
@@ -1,4 +1,4 @@
-document.getElementById('wel-btn').addEventListener('click', function(){
+/* document.getElementById('wel-btn').addEventListener('click', function(){
     const headerContainer = document.getElementById('header')
     const h1 = document.createElement('h1');
     h1.innerText = 'yeyo event a apnake sagotom';
@@ -8,3 +8,63 @@ document.getElementById('wel-btn').addEventListener('click', function(){
 document.getElementById('header').addEventListener('click', function(){
     document.body.style.backgroundColor = 'yellow'
 });
+ */
+
+// step 1: add event listener 
+/* document.getElementById('btn-post').addEventListener('click', function(){
+    // step 2: get the comment
+    const commentBox = document.getElementById('comment-box');
+    const newComment = commentBox.value;
+    
+    // step 3: create peragraph element
+    const commentContainer = document.getElementById('comment-container');
+    const p = document.createElement('p');
+    p.innerText = newComment;
+    commentContainer.appendChild(p)
+    commentBox.value = '';
+}) */
+
+
+// step 1 add even listener
+/* document.getElementById('btn-comment').addEventListener('click', function(){
+    // step 2 : com 
+    const commentBox = document.getElementById('comment-box');
+    const getComment =commentBox.value;
+    // set the value of new comment
+    const commentContainer = document.getElementById('comment-container')
+    const p = document.createElement('p');
+    p.innerText = getComment;
+    commentContainer.appendChild(p);
+    commentBox.value = '';
+    
+}) */
+
+
+document.getElementById('btn-more').addEventListener('click', function(){
+    console.log('yeoo');
+})
+
+
+// document.getElementById('input-field').addEventListener('focus', function(){
+//     console.log(' focus click');
+// })
+/* document.getElementById('input-field').addEventListener('blur', function(){
+    console.log(' blue click');
+}) */
+
+/* document.getElementById('input-field').addEventListener('keydown', function(e){
+    console.log(e.target.value);
+}) */
+
+
+/* document.getElementById('input-field').addEventListener('keypress', function(e){
+    console.log(e.target.value);
+}) */
+
+// document.getElementById('input-field').addEventListener('keyup', function(e){
+//     console.log(e.target.value);
+// })
+
+document.getElementById('btn-delete').addEventListener('click', function(){
+     
+})
