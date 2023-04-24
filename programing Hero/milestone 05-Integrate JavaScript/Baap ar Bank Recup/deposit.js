1fcc127500f72997e3c4fdd48054881f20b7f3ae
kawsarkabir <devkawsarkabir@gmail.com>
2023-04-25 00:11:28 +0600

    deposit done

--- programing Hero/milestone 05-Integrate JavaScript/Baap ar Bank Recup/deposit.js
+++ programing Hero/milestone 05-Integrate JavaScript/Baap ar Bank Recup/deposit.js
@@ -5,11 +5,20 @@ document.getElementById('deposit-btn').addEventListener('click', function(){
 
     const depositTotal = document.getElementById('deposit-total');
     const previousDepositTotalString = depositTotal.innerText;
-    const previousDepositTotal = parseInt(previousDepositTotalString)
+    const previousDepositTotal = parseInt(previousDepositTotalString);
     
     const currentDepositTotal = previousDepositTotal + newdepositAmount;
     depositTotal.innerText = currentDepositTotal;
 
 
+    // step 5: update balaance 
+    const balanceTotal = document.getElementById('balance-total');
+    const previousBalanceString = balanceTotal.innerText;
+    const previousBalance = parseInt(previousBalanceString);
+
+    // step 6: calculate balance
+    const currentBalanceTotal = previousBalance + newdepositAmount;
+    balanceTotal.innerText = currentBalanceTotal 
     depositField.value = ''
 });
+
