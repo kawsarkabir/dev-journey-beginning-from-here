d6fc355040db22a4ee344be0cf8ee9529d1d11c7
kawsarkabir <devkawsarkabir@gmail.com>
2023-04-25 12:43:32 +0600

    simple interactive bap ar bank done

---
+++ programing Hero/milestone 05-Integrate JavaScript/Baap ar Bank Recup/withdraw.js
@@ -0,0 +1,34 @@
+document.getElementById('withdraw-btn').addEventListener('click', function(){
+    const withdrawField = document.getElementById('withdraw-field');
+    const newWithdrawAmountString = withdrawField.value;
+    const newWithdrawAmount = parseInt(newWithdrawAmountString);
+
+    const withdrawTotal = document.getElementById('withdraw-total');
+    const previousWithdrawTotalString = withdrawTotal.innerText;
+    const previousWithdrawTotal = parseInt(previousWithdrawTotalString);
+    console.log(previousWithdrawTotal);
+
+    const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+    withdrawTotal.innerText = currentWithdrawTotal;
+
+
+    const balanceTotal = document.getElementById('balance-total');
+    const previousBalanceString = balanceTotal.innerText;
+    const previousBalance = parseInt(previousBalanceString);
+
+    const currentBalanceTotal = previousBalance - newWithdrawAmount;
+    balanceTotal.innerText = currentBalanceTotal;
+    
+
+
+
+
+
+
+
+
+
+    withdrawField.value = '';
+
+
+})
