7242f1de51a2f9269909e5e402a421cc70c129cd
kawsarkabir <devkawsarkabir@gmail.com>
2023-04-27 23:35:39 +0600

    functional bap ar bank done

---
+++ programing Hero/milestone 05-Integrate JavaScript/func baap ar bank/deposit.js
@@ -0,0 +1,45 @@
+//normal way 
+/* document.getElementById('deposit-btn').addEventListener('click', function(){
+    const depositField = document.getElementById('deposit-field');
+    const newDepositAmountString =depositField.value;
+    const newDepositAmount = parseInt(newDepositAmountString);
+    depositField.value = '';
+
+    
+    const depositTotal = document.getElementById('deposit-total');
+    const previousDepositTotalString = depositTotal.innerText;
+    const previousDepositTotal = parseInt(previousDepositTotalString);
+
+    const currentDepositTotal = previousDepositTotal + newDepositAmount;
+    depositTotal.innerText = currentDepositTotal;
+   
+
+    const balanceTotal = document.getElementById('balance-total');
+    const previousBalanceTotalString = balanceTotal.innerText;
+    const previousBalanceTotal = parseInt(previousBalanceTotalString);
+
+    const currentBalanceTotal = previousBalanceTotal + newDepositAmount;
+    balanceTotal.innerText = currentBalanceTotal;
+}) */
+// functional way
+
+function getInputValueById(inputId){
+    const inputField = document.getElementById(inputId);
+    const inputFieldValueString = inputField.value;
+    const inputFieldValue = parseInt(inputFieldValueString);
+    inputField.value = '';
+    return inputFieldValue;
+}
+
+function getElementValueById(elementId){
+    const element = document.getElementById(elementId);
+    const elementValueString = element.innerText;
+    const elementValue = parseInt(elementValueString);
+    return elementValue;
+}
+document.getElementById('deposit-btn').addEventListener('click' , function(){
+    const newDepositAmount = getElementValueById('deposit-field')
+    console.log(newDepositAmount);
+    const previousDepositTotal = getElementValueById('deposit-total');
+    console.log(previousDepositTotal);
+})
---
+++ programing Hero/milestone 05-Integrate JavaScript/func baap ar bank/withdraw.js
@@ -0,0 +1,21 @@
+document.getElementById('withdraw-btn').addEventListener('click' , function(){
+    const withdrawField = document.getElementById('withdraw-field');
+    const newWithdrawAmountString = withdrawField.value;
+    const newWithdrawAmount = parseInt(newWithdrawAmountString);
+    withdrawField.value = '';
+    
+    const withdrawTotal = document.getElementById('withdraw-total');
+    const previouswithdrawTotalString = withdrawTotal.innerText;
+    const previouswithdrawTotal = parseInt(previouswithdrawTotalString);
+    
+    const currentWithdrawTotal = previouswithdrawTotal + newWithdrawAmount;
+    withdrawTotal.innerText = currentWithdrawTotal;
+
+    const balanceTotal = document.getElementById('balance-total');
+    const previousBalanceTotalString = balanceTotal.innerText;
+    const previousBalanceTotal = parseInt(previousBalanceTotalString);
+
+    const currentBalanceTotal = previousBalanceTotal - newWithdrawAmount;
+    balanceTotal.innerText = currentBalanceTotal;
+
+});
