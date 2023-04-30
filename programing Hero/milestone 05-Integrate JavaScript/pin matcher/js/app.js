63389b8383bba2f9c1cf630c950e5ba30398d2d8
kawsarkabir <devkawsarkabir@gmail.com>
2023-04-30 13:43:03 +0600

    pin generate done

--- programing Hero/milestone 05-Integrate JavaScript/pin matcher/js/app.js
+++ programing Hero/milestone 05-Integrate JavaScript/pin matcher/js/app.js
@@ -1,1 +1,23 @@
- 
+function getPin(){
+    const pin = generatePin();
+    const pinString = pin + '';
+
+    if(pinString.length === 4){
+        return pin;
+    }else{
+        console.log('pin nnot 3digit found', pin);
+        return getPin();
+    }
+}
+
+function generatePin(){
+    const random = Math.round(Math.random()*10000);
+    return random;
+}
+
+document.getElementById('generate-pin').addEventListener('click' , function(){
+    const pin = getPin();
+    const displayPinField = document.getElementById('display-pin');
+    displayPinField.value = pin;
+
+})
