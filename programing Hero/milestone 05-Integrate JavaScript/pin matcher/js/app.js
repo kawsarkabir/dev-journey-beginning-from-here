bd673809f789766a94b2d20498de92fd8e83a848
kawsarkabir <devkawsarkabir@gmail.com>
2023-04-30 22:57:25 +0600

    clear btn work

--- programing Hero/milestone 05-Integrate JavaScript/pin matcher/js/app.js
+++ programing Hero/milestone 05-Integrate JavaScript/pin matcher/js/app.js
@@ -19,5 +19,22 @@ document.getElementById('generate-pin').addEventListener('click' , function(){
     const pin = getPin();
     const displayPinField = document.getElementById('display-pin');
     displayPinField.value = pin;
-
+})
+document.getElementById('calculator').addEventListener('click',function(e){
+    const number = (e.target.innerText);
+    const typeNumberField = document.getElementById('typed-numbers');
+    const previousTypeNumber = typeNumberField.value;
+    if(isNaN(number)){
+        if(number === 'C'){
+            typeNumberField.value = '';
+        }else if(number === '<'){
+            const digits = previousTypeNumber.split('');
+            digits.pop();
+            const remainingDigits = digits.join('');
+            typeNumberField.value = remainingDigits;
+        }
+    }else{
+        const newTypeNumber = previousTypeNumber + number;
+        typeNumberField.value = newTypeNumber;
+    }
 })
