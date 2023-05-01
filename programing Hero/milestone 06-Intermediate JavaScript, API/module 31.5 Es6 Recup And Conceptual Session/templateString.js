3fda67e67dce8fc5acebde5b791d345cd6cb0b23
kawsarkabir <devkawsarkabir@gmail.com>
2023-05-02 05:12:28 +0600

    template String

---
+++ programing Hero/milestone 06-Intermediate JavaScript, API/module 31.5 Es6 Recup And Conceptual Session/templateString.js
@@ -0,0 +1,13 @@
+// when ami template String ki ta jani na
+const sentence1 = 'amar name kawsar\n' + 'amar boyos 18.\n' + 'ami bekar';
+console.log(sentence1);
+
+const sentence2 = `amar name mehady.
+amar boyos 18.
+ami bekar`;
+console.log(sentence2);
+
+const name = 'k2';
+const age = 18;
+const profation = 'developer';
+console.log(`amar name ${name}. amar age ${age}. amar profation ${profation}`);
