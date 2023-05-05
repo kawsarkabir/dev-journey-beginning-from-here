fec4a08b9153fe9de1a40c457546a71b69ff3cd7
kawsarkabir <devkawsarkabir@gmail.com>
2023-05-05 12:35:48 +0600

    food project done

--- programing Hero/milestone 06-Intermediate JavaScript, API/module 33-API Examples and edge cases/meal.js
+++ programing Hero/milestone 06-Intermediate JavaScript, API/module 33-API Examples and edge cases/meal.js
@@ -1,22 +1,31 @@
-const loadMeal= ()=>{
-    fetch('https://www.themealdb.com/api/json/v1/1/search.php?f=a')
+const loadMeal= (search)=>{
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`)
     .then(res => res.json())
     .then(data => displayMeal(data.meals))
 }
-loadMeal();
+
 const displayMeal = meals =>{
-    const mealConteiner = document.getElementById('meal-container')
+    const mealConteiner = document.getElementById('meal-container');
+    mealConteiner.innerHTML = ''
     meals.forEach(meal =>{
-        console.log(meal);
         const mealDiv = document.createElement('div');
         mealDiv.classList.add('meal-items')
         mealDiv.innerHTML = `
         <img src="${meal.strMealThumb}">
-        <h2>Meal name: ${meal.strMeal}</h2>
-        <h3>idMeal: ${meal.idMeal}</h3>
-        <h4>Area: ${meal.strArea}</h4>
+        <h5>Meal name: ${meal.strMeal}</h5>
+        <h6>idMeal: ${meal.idMeal}</h6>
+        <h6>Area: ${meal.strArea}</h6>
 
         `
         mealConteiner.appendChild(mealDiv)
     })
 }
+const searchFood = () =>{
+    const searchField = document.getElementById('search-field');
+    const searchValue = searchField.value;
+    loadMeal(searchValue)
+    searchField.value = ''
+}
+
+
+// loadMeal('');
