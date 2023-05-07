5e1da0dde194ceca89d54bca14b0b78cc7cd3f23
kawsarkabir <devkawsarkabir@gmail.com>
2023-05-08 01:37:17 +0600

    API

---
+++ Anisul Bhai/React/day-01/src/components/Card.jsx
@@ -0,0 +1,15 @@
+function Card() {
+  return (
+    <section className="card-container">
+     <div>
+     <img src="" alt="" />
+      <h4>Hello, I am card</h4>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat, qui!
+      </p>
+     </div>
+    </section>
+  );
+}
+
+export default Card;
--- DevZero One/Fronted/React/ReactApp/src/App.jsx
+++ DevZero One/Fronted/React/ReactApp/src/App.jsx
@@ -1,11 +1,9 @@
-import Card from './components/Card.jsx';
 import './App.css'
 
 function App() {
   return (
      <div className="App">
-      <Card/>
-      <Card/>
+
      </div>
   );
 }
