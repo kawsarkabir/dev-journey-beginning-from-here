2ad250e371d4045f05ced42080dfec9de3e651dc
kawsarkabir <devkawsarkabir@gmail.com>
2023-05-14 03:12:05 +0600

    math function

---
+++ Stack Learner/Day-01/math-function.js
@@ -0,0 +1,33 @@
+// Math function
+console.log(Math.E);
+console.log(Math.PI);
+
+let x = 4.589
+// abs
+console.log(Math.abs(x));
+
+// flor
+console.log(Math.floor(x));
+
+// ceil
+console.log(Math.ceil(x));
+
+//round
+console.log(Math.round(x));
+
+// max
+console.log(Math.max(1, 2, 4, 5, 60));
+
+// min
+console.log(Math.min(190, 289, 90));
+
+
+// pow
+console.log(Math.pow(2, 3));
+
+// sqrt
+console.log(Math.sqrt(64));
+
+
+// random 
+console.log(Math.round(Math.random() * 50));
---
+++ Stack Learner/Day-01/statements.js
@@ -0,0 +1,7 @@
+console.log('Hello World');
+let str = 'string'
+let number = 10 + (30/5) * 5;
+
+let a = 10;
+let b = 20;
+let c = a>= b;
