9413bee8d3c5a8dd7b38a2c8b5a2447ec1d7823c
kawsarkabir <devkawsarkabir@gmail.com>
2023-05-14 00:47:16 +0600

    class-01 done

--- DevZero One/Fronted/JS/Lecture-01/app.js
+++ DevZero One/Fronted/JS/Lecture-01/app.js
@@ -26,3 +26,81 @@ console.log(b);
 
 // camer case variable name changing 
 let camelCase = 'kawsar kabir';
+
+// variable  
+// number
+const num1 = 10;
+const num2 = 90;
+ 
+// str
+const str1 = 'kawsar';
+const str2 = 'kabir';
+
+// bolean
+const bolean1 = true;
+const bolean2 = false;
+
+// variable naming convension 
+const alreadyCounterNumber = 80;
+
+
+
+// convert to str to number 
+const str3 = '10';
+const num3 = 90;
+console.log(Number(str3) + num3);
+
+
+//  parseInt && parseFloat
+const exam1 = '10';
+const exam2 = 90.1098;
+console.log(Number(exam1) + parseFloat(exam2.toFixed(2)));
+
+
+// math methods 
+// sqrt
+const num4 = 16.90;
+console.log(Math.sqrt(num4));
+
+// ceil
+console.log(Math.ceil(num4));
+
+
+// math.round 
+console.log(Math.round(num4));
+
+
+// math.random 
+
+// const number1 = 10;
+console.log(Math.round( Math.random() * 6));
+
+
+
+//  string methods
+let fName = 'kawsar';
+let lName = 'kabir';
+let fullName = fName + lName;
+// console.log(fullName.toUpperCase());
+console.log(fullName.toLocaleLowerCase());
+
+
+// date methods
+let date = new Date();
+console.log(date.getDate());
+console.log(date.getFullYear());
+
+
+// operator
+/* 
+1. Arithmetic Operator
+2. Comparison Operator
+3.Assingment Operator
+4. 
+*/
+
+// Arithmetic Operator 
+let num = 10;
+num++
+console.log(num);
+
