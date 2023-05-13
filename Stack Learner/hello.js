c3ed66ed526afc6ee7edfda452d2a8f9f30b7d12
kawsarkabir <devkawsarkabir@gmail.com>
2023-05-14 01:41:30 +0600

    variable Declare

---
+++ Stack Learner/hello.js
@@ -0,0 +1,5 @@
+console.log('hello world');
+console.log('my Fav no: ' + 9);
+
+console.log(9 + 9);
+console.log(9 + '9');
---
+++ Stack Learner/variable.js
@@ -0,0 +1,7 @@
+let name = 'kawsar kabir'
+let age = 18;
+
+console.log( name + ' knows Javascript');
+console.log('His age is only ' + age);
+console.log(name + ' is creating a javascript course');
+console.log('but his age is only ' + age);
