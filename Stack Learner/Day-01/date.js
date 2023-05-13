89b1d1de83255b33800094b51ea09e510912bf3b
kawsarkabir <devkawsarkabir@gmail.com>
2023-05-14 03:20:29 +0600

    day-01 done

---
+++ Stack Learner/Day-01/date.js
@@ -0,0 +1,8 @@
+let date = new Date()
+console.log(date.toDateString());
+console.log(date.toTimeString());
+console.log(date.toLocaleTimeString());
+
+console.log(date.getFullYear());
+console.log(date.getDate());
+console.log(date.getDay());
