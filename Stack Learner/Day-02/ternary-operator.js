fb90670684fb7cdbb079d52abc917540548dccc8
kawsarkabir <devkawsarkabir@gmail.com>
2023-05-14 21:09:01 +0600

    ternary operator

---
+++ Stack Learner/Day-02/ternary-operator.js
@@ -0,0 +1,14 @@
+// ternary operator
+let n = 13
+let str = ''
+
+if(n%2=== 0){
+    str = 'even';
+}else{
+    str = 'odd';
+}
+console.log(str);
+
+
+let result = n%2=== 0 ? 'EVEN': 'ODD'
+console.log(result);
