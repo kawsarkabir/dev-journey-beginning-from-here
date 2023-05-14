a0052a2e71208a0a4d26ce3dd1702d7271d6e98f
kawsarkabir <devkawsarkabir@gmail.com>
2023-05-14 23:16:00 +0600

    while Loop & do while loop

--- Stack Learner/Day-02/loop.js
+++ Stack Learner/Day-02/loop.js
@@ -26,9 +26,40 @@ for(let i = 1; i <=10; i++){
 // task 1-100 event number ar sumation
 let sum1  = 0;
 for(let i = 0; i<=10; i+=2){
-    console.log(sum1 + ' + ' + i + ' = ' + (sum1 + i));
+    // console.log(sum1 + ' + ' + i + ' = ' + (sum1 + i));
     sum1+=i
 }
 
 
 // ========= while Looppppppppppppppp
+
+let i = 0
+while(i <= 10){
+    // console.log(i, 'kawsar kabir');
+    i++
+}
+
+
+
+let isRunning = true
+while(isRunning){
+    let rand = Math.floor(Math.random()*10 + 1)
+    if (rand === 9){
+        console.log('winner winner chicken Dinner');
+        isRunning = false
+    }else{
+        console.log('You have got ' + rand);
+    }
+}
+
+
+
+// ============ do while loppppppppppp
+let isOk = false;
+while(isOk){
+    console.log('I am ok');
+}
+
+do{
+console.log('I am ok');
+}while(isOk)
