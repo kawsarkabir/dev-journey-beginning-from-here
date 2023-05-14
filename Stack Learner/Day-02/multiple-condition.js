fe778170e6c00fb2ed308bbea5c34511b242a519
kawsarkabir <devkawsarkabir@gmail.com>
2023-05-14 21:02:12 +0600

    multiple condition

---
+++ Stack Learner/Day-02/multiple-condition.js
@@ -0,0 +1,25 @@
+// multiple condition
+// && , ||
+
+const a = 10;
+const b = 20;
+const c = 30
+const d = 40
+if(a > b && c > d){
+    console.log('a is gether than B and c is geher then D');
+}else{
+    console.log('At leaset one conditon is false');
+}
+//  ||
+
+if(a > b || c > d){
+    console.log('a is gether than B and c is geher then D');
+}else{
+    console.log('At leaset one conditon is false');
+}
+
+
+// 
+
+let check = !(a>b)
+console.log(check);
