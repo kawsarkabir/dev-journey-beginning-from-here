e6ed8200c2ca484349e65d30daa9223812fb6c29
kawsarkabir <devkawsarkabir@gmail.com>
2023-05-14 23:01:53 +0600

    for loop

---
+++ Stack Learner/Day-02/loop.js
@@ -0,0 +1,34 @@
+//  There are Mainly Three Types of Loop 
+
+/* 
+1. For Loop
+2. While Loop
+3. Do While Loop
+*/
+ 
+// For Looooooooop
+for(let i = 1; i<=10; i++){
+    // console.log('kawsar kabir ' + i);
+}
+
+for(let i = 1; i<=10; i++){
+    // console.log('kawsar kabir ' + i);
+}
+
+
+//  sumation 
+let sum = 0;
+for(let i = 1; i <=10; i++){
+    // console.log(sum + ' + ' + i + ' = ' + (sum + i));
+    sum+= i
+}
+
+// task 1-100 event number ar sumation
+let sum1  = 0;
+for(let i = 0; i<=10; i+=2){
+    console.log(sum1 + ' + ' + i + ' = ' + (sum1 + i));
+    sum1+=i
+}
+
+
+// ========= while Looppppppppppppppp
