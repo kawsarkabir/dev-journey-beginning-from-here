b069b247919fe69d38d0ee0cc8050c3d81296c2a
kawsarkabir <devkawsarkabir@gmail.com>
2023-05-15 23:39:04 +0600

    array

---
+++ Stack Learner/Day-03/array.js
@@ -0,0 +1,13 @@
+// Array => how to declare array 
+const arr = [10, 20, 93, 34, 45, 60, 74,86,10];
+
+// find the index of 5
+console.log(arr[5]);
+
+// set the index
+arr[0] = 'kawsar'
+console.log(arr);
+
+
+// length find woth arry
+console.log(arr.length);
