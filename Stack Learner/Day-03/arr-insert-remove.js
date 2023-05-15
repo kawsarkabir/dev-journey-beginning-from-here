9b0b61c6c8a9a74c1a035c8a1117f4730953ef8f
kawsarkabir <devkawsarkabir@gmail.com>
2023-05-15 23:58:07 +0600

    array insert -remove

---
+++ Stack Learner/Day-03/arr-insert-remove.js
@@ -0,0 +1,20 @@
+// insert 9 to index 3
+let arr = [1, 2, 3,4, 5, 6, 7]
+
+// addded the lasst element
+arr.push(99)
+
+// added the fast Element
+arr.unshift(101)
+
+//  insert the new elemets|| 1st argu kon index a man bosabo 2nd holo kisu remove korte chai ki na 3rd holo ki bosabo
+arr.splice(3, 0, 9)
+
+// remove the last elements 
+arr.pop()
+
+// 1st the remove elements 
+arr.shift()
+
+
+console.log(arr);
