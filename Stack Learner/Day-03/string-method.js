376c7ca22cceaa71c7d539e1bd217301f52fb9d5
kawsarkabir <devkawsarkabir@gmail.com>
2023-05-15 22:29:00 +0600

    string methods

---
+++ Stack Learner/Day-03/string-method.js
@@ -0,0 +1,16 @@
+let a = 'I will Become a '
+let b = 'Full Stack Developer'
+let ab = a.concat(b)
+console.log(ab);
+
+console.log(ab.charAt(0));
+
+// to upperCase lowerCase
+console.log(a.toUpperCase());
+console.log(b.toLowerCase());
+
+// speac remove
+console.log('      Hello World'.trim());
+
+// split
+console.log(ab.split(''));
