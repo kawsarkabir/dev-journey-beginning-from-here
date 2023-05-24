6f88b5cfc0bbbe3a8cb70d59ce1b2b7e58deef81
kawsarkabir <devkawsarkabir@gmail.com>
2023-05-24 23:52:05 +0600

    counter ap

--- programing Hero/milestone 08- Simple React/module 45-react core concept JSX, props, state/react-core-concept-recup/src/App.jsx
+++ programing Hero/milestone 08- Simple React/module 45-react core concept JSX, props, state/react-core-concept-recup/src/App.jsx
@@ -1,6 +1,31 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  return (
+    <div className="App">
+      <Counter />
+    </div>
+  );
+}
+
+function Counter() {
+  const [count, setCount] = useState(23);
+  const IncreaseCount =() => setCount(count+1);
+  const decreaseCount =() => setCount(count-1);
+  return(
+    <div className="counter-container">
+    <h1>Count:{count}</h1>
+    <button onClick={IncreaseCount} >Increase:{setCount}</button>
+    <button onClick={decreaseCount}>Decrease</button>
+  </div>
+  )
+}
+
+export default App;
+
+/* 
+
   const products = [
     { name: "laptop", price: 290 },
     { name: "phone", price: 350 },
@@ -10,22 +35,19 @@ function App() {
     { name: "tablet", price: 450 },
   ];
 
-  return (
-    <div className="App">
-      {products.map((product) => (
+
+ ===== react ar core concept here ==  
+ 
+{products.map((product) => (
         <Products name={product.name} price={product.price} />
       ))}
-    </div>
-  );
-}
 
-function Products(props) {
+  function Products(props) {
   return (
     <div className="product-container">
       <h3>Name:{props.name}</h3>
       <p>Price:{props.price}</p>
     </div>
   );
-}
-
-export default App;
+}    
+*/
