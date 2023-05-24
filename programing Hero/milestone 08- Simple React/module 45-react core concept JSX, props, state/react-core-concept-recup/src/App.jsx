66310ced062b9b306e891c4f8cf4f2c0bc7f3792
kawsarkabir <devkawsarkabir@gmail.com>
2023-05-25 00:52:18 +0600

    dynamic data load with useState & useEfect

--- programing Hero/milestone 08- Simple React/module 45-react core concept JSX, props, state/react-core-concept-recup/src/App.jsx
+++ programing Hero/milestone 08- Simple React/module 45-react core concept JSX, props, state/react-core-concept-recup/src/App.jsx
@@ -1,24 +1,53 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
+ 
 
 function App() {
   return (
     <div className="App">
-      <Counter />
+      <ExternalUsers />
     </div>
   );
 }
 
-function Counter() {
-  const [count, setCount] = useState(23);
+/* function Counter() {
+  const [count, setCount] = useState(0);
   const IncreaseCount =() => setCount(count+1);
   const decreaseCount =() => setCount(count-1);
   return(
     <div className="counter-container">
     <h1>Count:{count}</h1>
-    <button onClick={IncreaseCount} >Increase:{setCount}</button>
+    <button onClick={IncreaseCount} >Increase</button>
     <button onClick={decreaseCount}>Decrease</button>
   </div>
+  )
+} */
+
+// Daynamic data load
+function ExternalUsers(){
+  //step 1: useState 
+  const [users, setUsers] = useState([])
+  // step 2: useEfect 
+   useEffect( ()=>{
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data => setUsers(data))
+   }, []);
+    return(
+      <div>
+        {
+          users.map(user=><User name ={user.name} email = {user.email} />)
+        }
+      </div>
+    )
+}
+
+function User(props){
+  return(
+    <div className="user-container">
+     <h3>Name: {props.name}</h3>
+  <p>Email: {props.email}</p>
+   </div>
   )
 }
 
