fb2eacbc60a8883d6699501ac36fdfa269a9681a
kawsarkabir <devkawsarkabir@gmail.com>
2023-06-11 00:19:43 +0600

    save data in local strorage

--- programing Hero/milestone 08- Simple React/module-48 ema-john-ecomerce/ema-john/src/components/Shop/Shop.jsx
+++ programing Hero/milestone 08- Simple React/module-48 ema-john-ecomerce/ema-john/src/components/Shop/Shop.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Card from "../Card/Card";
 import Cart from "../Cart/Cart";
+import { addToDb, getShoppingCart } from "../../utilities/fakedb";
 
 const Shop = () => {
   const [products, setProducts] = useState([]);
@@ -13,9 +14,24 @@ const Shop = () => {
       .then((data) => setProducts(data));
   }, []);
 
+  useEffect(()=>{
+    const storedCart = getShoppingCart()
+    const saveCart = [];
+    for(const id in storedCart){
+      const addedProduct = products.find(product =>product.id === id)
+      if(addedProduct){
+        const qunantity = storedCart[id]
+        addedProduct.quantity = qunantity
+        saveCart.push(addedProduct)
+      }
+    }
+    setCart(saveCart)
+  },[products])
+
   const handleAddToCart = (product) => {
     const newCart = [...cart, product];
     setCart(newCart);
+    addToDb(product.id)
   };
   return (
     <div className="shop-container">
