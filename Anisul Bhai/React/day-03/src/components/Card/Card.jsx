52b56dfba78b1cafa803b63f17287b70dbfc632b
kawsarkabir <devkawsarkabir@gmail.com>
2023-06-14 23:48:51 +0600

    mapping

---
+++ Anisul Bhai/React/day-03/src/components/Card/Card.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+
+const todos=[
+    {
+        title:'todo1',
+        dec:'Hello I am todo-1 Description.'
+    },
+    {
+        title:'todo2',
+        dec:'Hello I am todo-2 Description.'
+    },
+    {
+        title:'todo3',
+        dec:'Hello I am todo-3 Description.'
+    },
+]
+
+const Card = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Card;
+ 
